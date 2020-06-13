8cc2ceabf769927b83f79bf59484f070271dfa8c
hemali <hemalidodia1310@gmail.com>
2020-06-13 16:48:53 +0530

    updated for loop programs

--- for_loop_programs/harmonic_number.sh
+++ for_loop_programs/harmonic_number.sh
@@ -2,14 +2,14 @@
 
 read -p "enter value till harmonic number is needed: " n;
 function harmonic(){
-START=1
-sum=0
-for (( j=1; j<=$n; j++ ))
-do
-	div=`echo $START $j | awk '{num1=$2;num2=$1; print num2/num1}'`
-	sum=`echo $sum $div | awk '{num1=$1;num2=$2; print num1+num2}'`
-done
-echo $sum
+	START=1
+	sum=0
+	for (( j=1; j<=$n; j++ ))
+	do
+		div=`echo $START $j | awk '{num1=$2;num2=$1; print num2/num1}'`
+		sum=`echo $sum $div | awk '{num1=$1;num2=$2; print num1+num2}'`
+	done
+	echo $sum
 }
 
 harmonic_number=$( harmonic $n )
