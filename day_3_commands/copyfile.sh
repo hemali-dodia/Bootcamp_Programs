928f4aa224c28dc0a2c45d779888b63effd585a2
hemali <hemalidodia1310@gmail.com>
2020-06-13 13:06:05 +0530

    copy files in to folder name same as file name

---
+++ day_3_commands/copyfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+for files in `ls *.txt`;
+do
+	foldername=`echo $files | awk -F. '{print $1}'`;
+	#echo "checking for directories existed";
+	if [ -d $foldername ]
+	then
+		rm -r $foldername;
+	fi
+	#echo creating folder $foldername;
+	mkdir $foldername;
+	#echo copy $files to $foldername;
+	cp $files $foldername;
+done
