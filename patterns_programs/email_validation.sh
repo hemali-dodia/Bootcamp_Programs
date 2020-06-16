4175ca561e7852f6b7421d42c3c9239caa87dacf
hemali <hemalidodia1310@gmail.com>
2020-06-16 17:10:49 +0530

    validates email adress only

---
+++ patterns_programs/email_validation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+shotp -s extglob
+
+emailpattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+read -p "enter your email: " email
+if [[ $email =~ $emailpattern ]]
+then
+	echo "valid email"
+else
+	echo "invalid email"
+fi
