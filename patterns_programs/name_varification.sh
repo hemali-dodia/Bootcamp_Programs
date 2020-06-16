25eeca9e5bfde053e0fbe3ad27e44f330daa4336
hemali <hemalidodia1310@gmail.com>
2020-06-16 17:51:02 +0530

    varifies user's name

---
+++ patterns_programs/name_varification.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+shopt -s extglob
+
+function valFirstLastName(){
+        local Name=$1
+        F_L_Pattern="^[A-Z][a-z]{2,}$"
+        if [[ $Name =~ $F_L_Pattern ]]
+        then
+                echo "Accepted"
+        else
+                echo "Invalid Name, 1st char should be cap, min 3 char required"
+        fi
+}
+
+read -p "enter first name and last name: " First_name Last_name;
+
+firstName="$( valFirstLastName $First_name )"
+lastName="$( valFirstLastName $Last_name )"
+
