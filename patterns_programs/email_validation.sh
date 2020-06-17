9e03ae90426df5eb1568a0ee905ab4aab64607ba
hemali <hemalidodia1310@gmail.com>
2020-06-17 09:22:27 +0530

    email varification ending with .abc.in

--- patterns_programs/email_validation.sh
+++ patterns_programs/email_validation.sh
@@ -3,6 +3,7 @@
 shotp -s extglob
 
 emailpattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+
 read -p "enter your email: " email
 if [[ $email =~ $emailpattern ]]
 then
