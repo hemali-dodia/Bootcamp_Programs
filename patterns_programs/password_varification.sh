3a535d70d42904766c6e8486f5c6c1da2caaf94a
hemali <hemalidodia1310@gmail.com>
2020-06-17 10:05:01 +0530

    varifies password w.r.t length, special char, upper/lower case

--- patterns_programs/password_varification.sh
+++ patterns_programs/password_varification.sh
@@ -2,15 +2,32 @@
 shopt -s extglob
 function passwordVal(){
         local pw=$1
-        passPattern="([A-Za-z0-9]){8,}$"
-        if [[ $pw =~ $passPattern ]]
+        passPattern1="^([A-Za-z0-9@#!]){8,}$"
+	passPattern2="^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$"
+	passPattern3="^[a-zA-Z0-9]*[0-9]+[a-zA-Z@#!]*$"
+	passPattern4="^([a-zA-Z0-9]*)[^a-zA-Z_0-9\ ]([a-zA-Z0-9]*)$"
+        if [[ $pw =~ $passPattern1 ]]
         then
-                echo "valid password"
-        else
-                echo "invalid password"
-        fi
+		if [[ $pw =~ $passPattern2 ]]
+		then
+
+			if [[ $pw =~ $passPattern3 ]]
+        	        then
+				if [[ $pw =~ $passPattern4 ]]
+				then
+					echo "valid password"
+				else
+       		 	        	echo "invalid password"
+	        		fi
+			else
+				echo "invalid3"
+			fi
+		else
+			echo "invalid2"
+		fi
+	else
+		echo "invalid1"
+	fi
 }
 read -p "enter password: " password;
 pswd="$( passwordVal $password )"
-
-
