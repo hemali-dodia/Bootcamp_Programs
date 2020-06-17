01e5ba974e7e7b76e449c7ed31568ca757a0400a
hemali <hemalidodia1310@gmail.com>
2020-06-17 10:06:04 +0530

    user details verification program

--- patterns_programs/user_registration.sh
+++ patterns_programs/user_registration.sh
@@ -49,15 +49,33 @@ read -p "enter mob number with country code: " Code Number;
 mob_num="$( ValMobileNumber $Code $Number )"
 
 function passwordVal(){
-	local pw=$1
-	passPattern="([A-Za-z0-9]){8,}$"
-	if [[ $pw =~ $passPattern ]]
-	then
-		echo "valid password"
-	else
-		echo "invalid password"
-	fi
+        local pw=$1
+        passPattern1="^([A-Za-z0-9@#!]){8,}$"
+        passPattern2="^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$"
+        passPattern3="^[a-zA-Z0-9]*[0-9]+[a-zA-Z@#!]*$"
+        passPattern4="^([a-zA-Z0-9]*)[^a-zA-Z_0-9\ ]([a-zA-Z0-9]*)$"
+        if [[ $pw =~ $passPattern1 ]]
+        then
+                if [[ $pw =~ $passPattern2 ]]
+                then
+
+                        if [[ $pw =~ $passPattern3 ]]
+                        then
+                                if [[ $pw =~ $passPattern4 ]]
+                                then
+                                        echo "valid password"
+                                else
+                                        echo "invalid password"
+                                fi
+                        else
+                                echo "invalid3"
+                        fi
+                else
+                        echo "invalid2"
+                fi
+        else
+                echo "invalid1"
+        fi
 }
 read -p "enter password: " password;
 pswd="$( passwordVal $password )"
-
