809716e91b987efde2c1d854892be685c5a65047
hemali <hemalidodia1310@gmail.com>
2020-06-20 09:04:16 +0530

    password verification(working properly)

--- patterns_programs/password_varification.sh
+++ patterns_programs/password_varification.sh
@@ -4,7 +4,7 @@ function passwordVal(){
         local pw=$1
         passPattern1="^([A-Za-z0-9@#!]){8,}$"
 	passPattern2="^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$"
-	passPattern3="^[a-zA-Z0-9]*[0-9]+[a-zA-Z@#!]*$"
+	passPattern3="^[a-zA-Z0-9]*[0-9]+[a-z0-9A-Z@#!]*$"
 	passPattern4="^([a-zA-Z0-9]*)[^a-zA-Z_0-9\ ]([a-zA-Z0-9]*)$"
         if [[ $pw =~ $passPattern1 ]]
         then
@@ -20,13 +20,13 @@ function passwordVal(){
        		 	        	echo "invalid password"
 	        		fi
 			else
-				echo "invalid3"
+				echo "error: enter at least 1 number"
 			fi
 		else
-			echo "invalid2"
+			echo "error: enter at least one special character"
 		fi
 	else
-		echo "invalid1"
+		echo "error: length should be at least 8 characters"
 	fi
 }
 read -p "enter password: " password;
