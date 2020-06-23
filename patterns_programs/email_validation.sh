611e33ee63e75ef37c517355c40aa71e37f92e14
hemali <hemalidodia1310@gmail.com>
2020-06-23 15:09:03 +0530

    email validationworking on bash shell with regex pattern

--- patterns_programs/email_validation.sh
+++ patterns_programs/email_validation.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -x
 
 shotp -s extglob
-
-emailpattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}$"
+function email_varification()
+{
+	emailpattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+\.[0-9a-zA-Z]{2,4}$"
+	if [[ $email =~ $emailpattern ]]
+	then
+		echo "valid email"
+	else
+		echo "invalid email"
+	fi
+}
 read -p "enter your email: " email
-if [[ $email =~ $emailpattern ]]
-then
-	echo "valid email"
-else
-	echo "invalid email"
-fi
+email_varification $email
